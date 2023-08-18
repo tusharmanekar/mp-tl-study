a72d333dbc208b7d1f493e8ea0528ff232561f7c
root <root@laptop.localdomain>
2023-08-18 16:59:32 +0200

    skeleton

---
+++ utils.py
@@ -0,0 +1,124 @@
+import numpy as np
+
+import torch
+import torch.nn as nn
+import torch.nn.functional as F
+import torch.optim as optim
+from torchvision import datasets, transforms
+
+# init given linear layer m with given sw and sb
+def init_weights(m, sw, sb):
+    if type(m) == nn.Linear:
+        nn.init.normal_(m.weight, mean=0.0, std=(np.sqrt(sw / m.out_features)))
+        nn.init.normal_(m.bias, mean=0.0, std=np.sqrt(sb))
+
+# init new model
+# TODO: activation function
+# TODO: name the layers
+def generate_fc_dnn(input_dim, output_dim, depth, width):
+    def gen_linear_layer_dim(layer_index):
+        return {
+            0: (input_dim, width),
+            depth - 1: (width, output_dim),
+        }.get(layer_index, (width, width))
+
+    fc_list = list()
+    for i in range(depth):
+        fc_list += [
+            nn.Linear(*gen_linear_layer_dim(i)),
+            nn.LogSoftmax(dim=1) if (depth - 1 == i) else nn.Tanh()
+        ]
+    return nn.Sequential(*fc_list)
+
+# dataset_loader is fine-tuning dataset
+def eval(model, device, dataset_loader, debug):
+    model.eval()
+    test_loss, correct = 0., 0.
+    with torch.no_grad():
+        for data, target in dataset_loader:
+            data, target = data.reshape([data.shape[0],
+                                         -1]).to(device), target.to(device)
+            output = model(data)
+            test_loss += F.nll_loss(
+                output, target, reduction='sum').item()  # sum up batch loss
+            pred = output.argmax(
+                dim=1,
+                keepdim=True)  # get the index of the max log-probability
+            correct += pred.eq(target.view_as(pred)).sum().item()
+
+    num_data = len(dataset_loader.dataset)
+    test_loss /= num_data
+    acc = correct / num_data
+    if debug:
+        print('\nTrain set: Average loss: {:.4f}, Accuracy: {}/{} ({:.0f}%)\n'.
+              format(test_loss, correct, num_data, 100. * acc))
+
+    return acc
+
+
+class MNISTtrainer(object):
+    def __init__(self, batch_size):
+        self.input_dim = 28 * 28
+        self.output_dim = 10
+
+        # rescale to [-.5, .5]
+        self.loader = torch.utils.data.DataLoader(
+            datasets.MNIST(
+                '../data',
+                train=True,
+                download=True,
+                transform=transforms.Compose([
+                    transforms.ToTensor(),
+                    transforms.Lambda(lambda x: (x * 2 - 1) * 0.5),
+                ])),
+            batch_size=batch_size,
+            shuffle=True)
+
+
+# TODO: option to freeze some layers
+# TODO: option to save the model?
+def compute_training_acc(model, dataset, params, debug=False):
+
+    device = torch.device(params['device'])
+    model.apply(lambda m: init_weights(m, params['sw'], params['sb']))
+    optimizer = optim.SGD(model.parameters(), lr=params['lr'])
+    if debug: print(model, optimizer)
+
+    # run training for few steps and return the accuracy
+    train_acc = 0.0
+    model.train()
+    for batch_idx, (data, target) in enumerate(dataset.loader):
+        data, target = data.reshape([data.shape[0],
+                                     -1]).to(device), target.to(device)
+        optimizer.zero_grad()
+        output = model(data)
+        loss = F.nll_loss(output, target)
+        loss.backward()
+        optimizer.step()
+
+        if debug and batch_idx % 20 == 0:
+            print('Train step: {} \tLoss: {:.6f}'.format(
+                batch_idx, loss.item()))
+        if (batch_idx == params['num_train']):
+            train_acc = eval(model, device, dataset.loader, debug)
+            break
+
+    return train_acc, model
+
+# cut_point: no. of layers to keep in the model
+# reinitialize after cutting point using init_weights function
+def cut_model(model, sw, sb, cut_point=1):
+    for layer in model.layers:
+
+# Replicate the pretrained model, cut at each layer and fine-tune, 
+# return a list of accuracies (and some more results) for each layers.
+def calculate_cut_accuracies(model, sw, sb, dataset, params):
+    train_accuracies = []
+    for i, layer in enumerate(model.layers):
+        model_tmp = cut_model(model, sw, sb, cut_point=i)
+        # TODO: don't forget to freeze the layers
+        acc, model_tmp_fine_tuned = compute_training_acc(dataset, params, debug=False)
+        train_accuracies.append(acc)
+    return train_accuracies
+
+# TODO: Add other util functions: plotting training etc

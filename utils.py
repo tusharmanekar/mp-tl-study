802450387443065dec4bb5049f77c90b722c32d4
davidguzmanp <43582853+davidguzmanp@users.noreply.github.com>
2023-08-19 16:55:45 +0200

    experiments of transferrabilty fashion to mnist: dense vs cnn

--- utils.py
+++ utils.py
@@ -31,6 +31,77 @@ def generate_fc_dnn(input_dim, output_dim, depth, width):
         ]
     return nn.Sequential(*fc_list)
 
+import torch.nn as nn
+
+def generate_cnn(input_dim, output_dim, depth, num_channels):
+    """
+    Generate a CNN model.
+
+    Parameters:
+    - input_dim (int): Height/Width of square input image.
+    - output_dim (int): Number of output classes.
+    - depth (int): Number of convolutional layers.
+    - num_channels (int): Number of channels for the first convolutional layer. 
+                          This will be doubled after each layer for the demonstration.
+
+    Returns:
+    - model (nn.Sequential): CNN model.
+    """
+
+    layers = []
+    in_channels = 1  # Assuming grayscale input images
+
+    # Convolutional layers
+    for i in range(depth):
+        layers.append(nn.Conv2d(in_channels, num_channels, kernel_size=3, padding=1))
+        layers.append(nn.Tanh())
+        layers.append(nn.MaxPool2d(2))
+        in_channels = num_channels
+        #num_channels *= 2
+
+        # Adjust input_dim due to max pooling
+        input_dim = input_dim // 2
+
+    # Flatten the output from convolutional layers
+    layers.append(nn.Flatten())
+
+    # Fully connected layer for classification
+    layers.append(nn.Linear(in_channels * input_dim * input_dim, output_dim))
+    layers.append(nn.LogSoftmax(dim=1))
+
+    model = nn.Sequential(*layers)
+    return model
+
+'''def generate_cnn(input_dim, output_dim, depth, num_channels):
+    layers = []
+    in_channels = 1  # Assuming grayscale input images
+
+    # Convolutional layers
+    for i in range(depth):
+        layers.append(nn.Conv2d(in_channels, num_channels, kernel_size=3 if i > 0 else 5, padding=1))
+        layers.append(nn.Tanh())
+        
+        # Add MaxPool2d layer every 2 convolutional layers
+        if i % 2 == 1:
+            layers.append(nn.MaxPool2d(2))
+            input_dim = input_dim // 2
+        
+        in_channels = num_channels
+        num_channels *= 2
+
+    # Flatten the output from convolutional layers
+    layers.append(nn.Flatten())
+
+    # Fully connected layer for classification
+    layers.append(nn.Linear(in_channels * input_dim * input_dim, output_dim))
+    layers.append(nn.LogSoftmax(dim=1))
+
+    model = nn.Sequential(*layers)
+    return model
+'''
+
+
+
 # dataset_loader is fine-tuning dataset
 def eval(model, device, dataset_loader, debug):
     model.eval()
@@ -75,6 +146,26 @@ class MNISTtrainer(object):
             batch_size=batch_size,
             shuffle=True)
         
+
+class MNISTtrainerCNN(object):
+    def __init__(self, batch_size):
+        self.input_dim = (1, 28, 28)  # Channels x Height x Width
+        self.output_dim = 10
+
+        # rescale to [-.5, .5]
+        self.loader = torch.utils.data.DataLoader(
+            datasets.MNIST(
+                '../data',
+                train=True,
+                download=True,
+                transform=transforms.Compose([
+                    transforms.ToTensor(),
+                    transforms.Lambda(lambda x: (x * 2 - 1) * 0.5),
+                ])),
+            batch_size=batch_size,
+            shuffle=True)
+
+        
 class FashionMNISTtrainer(object):
     def __init__(self, batch_size):
         self.input_dim = 28 * 28
@@ -93,6 +184,24 @@ class FashionMNISTtrainer(object):
             batch_size=batch_size,
             shuffle=True)
 
+
+class FashionMNISTtrainerCNN(object):
+    def __init__(self, batch_size):
+        self.input_dim = (1, 28, 28)  # Channels x Height x Width
+        self.output_dim = 10
+
+        # rescale to [-.5, .5]
+        self.loader = torch.utils.data.DataLoader(
+            datasets.FashionMNIST(
+                '../data',
+                train=True,
+                download=True,
+                transform=transforms.Compose([
+                    transforms.ToTensor(),
+                    transforms.Lambda(lambda x: (x * 2 - 1) * 0.5),
+                ])),
+            batch_size=batch_size,
+            shuffle=True)
 
 
 # TODO: option to freeze some layers
@@ -160,6 +269,59 @@ def compute_training_acc_epochs(model, dataset, params, debug=False):
     train_acc = eval(model, device, dataset.loader, debug)
     return train_acc, model
 
+def eval_cnn(model, device, dataset_loader, debug):
+    model.eval()
+    test_loss, correct = 0., 0.
+    with torch.no_grad():
+        for data, target in dataset_loader:
+            data, target = data.to(device), target.to(device)
+            output = model(data)
+            test_loss += F.nll_loss(output, target, reduction='sum').item()  
+            pred = output.argmax(dim=1, keepdim=True)
+            correct += pred.eq(target.view_as(pred)).sum().item()
+
+    num_data = len(dataset_loader.dataset)
+    test_loss /= num_data
+    acc = correct / num_data
+    if debug:
+        print('\nTrain set: Average loss: {:.4f}, Accuracy: {}/{} ({:.0f}%)\n'.format(test_loss, correct, num_data, 100. * acc))
+
+    return acc
+
+def compute_training_acc_epochs_cnn(model, dataset, params, debug=False):
+    device = torch.device(params['device'])
+    model.apply(lambda m: init_weights(m, params['sw'], params['sb']))
+    optimizer = optim.SGD(model.parameters(), lr=params['lr'])
+    
+    if debug: 
+        print(model, optimizer)
+
+    train_acc = 0.0
+    model.train()
+
+    # Loop over epochs
+    for epoch in range(params['num_train']):
+        for batch_idx, (data, target) in enumerate(dataset.loader):
+            data, target = data.to(device), target.to(device)
+            optimizer.zero_grad()
+            output = model(data)
+            loss = F.nll_loss(output, target)
+            loss.backward()
+            optimizer.step()
+
+            if debug and batch_idx % 20 == 0:
+                pass
+
+        # Evaluate after each epoch
+        if debug:
+            train_acc = eval_cnn(model, device, dataset.loader, debug)
+            print('Epoch: {} \tTraining Accuracy: {:.2f}%'.format(epoch, train_acc*100))
+
+    # Final evaluation after all epochs are completed
+    train_acc = eval_cnn(model, device, dataset.loader, debug)
+    return train_acc, model
+
+
 
 # cut_point: no. of layers to keep in the model
 # reinitialize after cutting point using init_weights function
@@ -196,6 +358,53 @@ def cut_model(model, sw = 1, sb = 1, cut_point=1, freeze=True):
     # Return new model
     return nn.Sequential(*new_layers)
 
+def cut_cnn_model(model, cut_point=1, freeze=True):
+    """
+    Cut the CNN model at a specific layer and reinitialize the weights for layers after cut_point.
+
+    Parameters:
+    - model (nn.Module): Original model.
+    - cut_point (int): Layer index at which to cut the model.
+    - freeze (bool): If True, layers before cut_point will have their weights frozen.
+
+    Returns:
+    - new_model (nn.Sequential): Cut and potentially modified model.
+    """
+    
+    # Deep copy to avoid changing the original model
+    model = copy.deepcopy(model)
+
+    # Convert sequential model to list of layers
+    layers = list(model.children())
+
+    # Check if cut_point is out of range
+    if cut_point < 0 or cut_point >= len(layers):
+        raise ValueError("cut_point should be in range [0, number of layers - 1]")
+
+    # If freeze is True, set requires_grad to False for layers before cut_point
+    if freeze:
+        for i in range(cut_point):
+            for param in layers[i].parameters():
+                param.requires_grad = False
+
+    # Cut layers
+    new_layers = layers[:cut_point]
+
+    # Reinitialize layers after cut point
+    for i in range(cut_point, len(layers)):
+        layer = layers[i]
+        
+        # Reinitialize weights if layer has parameters (like Conv2d)
+        if hasattr(layer, 'weight'):
+            nn.init.kaiming_uniform_(layer.weight, a=0, mode='fan_in', nonlinearity='relu')
+            if layer.bias is not None:
+                nn.init.constant_(layer.bias, 0)
+        
+        # Append to new layers
+        new_layers.append(layer)
+
+    # Return new model
+    return nn.Sequential(*new_layers)
 
 
 # Replicate the pretrained model, cut at each layer and fine-tune, 

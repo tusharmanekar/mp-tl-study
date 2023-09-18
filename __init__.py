c2625feba3dd73fc38a0c196ba8516a5abfb338b
arnisafazla <fazlarnisa@gmail.com>
2023-09-18 11:53:16 +0200

    reverting

--- __init__.py
+++
@@ -1,7 +0,0 @@
-#!/usr/bin/env python3
-# -*- coding: utf-8 -*-
-
-from mp-tl-study/utils import *
-from mp-tl-study/data_utils import *
-from mp-tl-study/plots import *
-from mp-tl-study/metrics import *
